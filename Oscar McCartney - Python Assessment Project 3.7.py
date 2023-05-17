794c72965e2bd1b2a04d3320f14c948f79342707
0scar-M <133919624+0scar-M@users.noreply.github.com>
2023-05-18 10:07:02 +1200

    Update Oscar McCartney - Python Assessment Project 3.7.py
    Added axis lines.

--- Oscar McCartney - Python Assessment Project 3.7.py
+++ Oscar McCartney - Python Assessment Project 3.7.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter.ttk import *
 import os
 
 class GrapherGUI:
@@ -12,9 +13,11 @@ class GrapherGUI:
         """
 
         self.canvas = Canvas(root, width = 512, height = 512)
+        x = self.canvas.create_line(0, 256, 512, 256)
+        y = self.canvas.create_line(256, 0, 256, 512)
         self.canvas.grid()
 
-        self.entry = Entry(root, width = 30, font = ("Cambria",24))
+        self.entry = Entry(root, width = 32, font = ("Times New Roman Italic", 24), )
         self.entry.grid(row = 1)
 
 
